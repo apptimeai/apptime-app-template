b5c59a1da2bc9cdd2532c732a50ab0dd3d8b94a7
Akira <gustavomaedoyt@gmail.com>
2026-01-30 00:13:24 -0300

    feat: Set up initial Nuxt.js template with Bun, Tailwind CSS, and TypeScript.

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/main.css'],
+  ssr: false,
+  nitro: {
+    preset: 'static'
+  }
+})
